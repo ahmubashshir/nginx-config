a393fe7625fd0ec685cdbbd6197caac226f15ae6
Mubashshir <ahmubashshir@gmail.com>
2021-02-27 02:53:34 +0600

    update spa file manager

--- static/js/file.js
+++ static/js/file.js
@@ -1,4 +1,7 @@
 /*jshint esversion: 6 */
+"use strict";
+
+var req = undefined;
 
 var addEvent = ( function () {
 	if ( document.addEventListener ) {
@@ -27,83 +30,136 @@ var addEvent = ( function () {
 } )();
 
 function updateCrumbs () {
-	window.document.title = "Files: " + decodeURI( window.location.pathname );
-	setTimeout( function () {
-		var loc = window.location.pathname;
-		var link;
-		var segments = loc.split( '/' );
-		var breadcrumbs = '';
-		var currentPath = '/';
-		for ( var i = 0; i < segments.length; i++ ) {
-			if ( segments[ i ] !== '' ) {
-				currentPath += segments[ i ] + '/';
-				if ( segments.length > i + 2 ) {
-					link = '<a href="' + currentPath + '">' + decodeURIComponent( segments[ i ] ) + '<\/a>';
-				} else {
-					link = decodeURIComponent( segments[ i ] );
-				}
-				breadcrumbs += '<li class="breadcrumb-item' + ( ( segments.length == i + 2 ) ? ' active fg-active' : '' ) + '">' + link + '<\/li>';
-			} else if ( segments.length - 1 !== i ) {
-				currentPath += '';
-				breadcrumbs += '<li class="breadcrumb-item">Root<\/li>';
+	let breadcrumb = document.getElementById( 'breadcrumbs' );
+	let path = window.location.pathname;
+	if ( path.endsWith( "/" ) )
+		window.document.title = "Files: " + decodeURI( path );
+	else
+		window.document.title = "View: " + decodeURI( path );
+
+	if ( breadcrumb ) {
+		setTimeout( function () {
+			let loc = window.location.pathname;
+			let link, crumb;
+			let segments = loc.split( '/' );
+			let breadcrumbs = '';
+			let currentPath = '/';
+			if ( segments[ segments.length - 1 ] == "" ) {
+				segments.splice( segments.length - 1, segments.length );
 			}
-		}
-		document.getElementById( 'breadcrumbs' ).innerHTML = breadcrumbs;
-		addEvent( document.getElementById( 'list' ), 'click', navigate );
-		addEvent( document.getElementById( 'breadcrumbs' ), 'click', navigate );
-	}, 500 );
+
+			for ( var i = 0; i < segments.length; i++ ) {
+				crumb = document.createElement( 'li' );
+				crumb.classList.add( "breadcrumb-item" )
+				if ( i == 0 ) {
+					crumb.append( "Root" );
+					breadcrumbs += crumb.outerHTML;
+				} else if ( i > 0 ) {
+					currentPath += segments[ i ] + '/';
+					if ( i < segments.length - 1 ) {
+						link = document.createElement( 'a' );
+						link.href = currentPath;
+						link.text = decodeURIComponent( segments[ i ] );
+					} else {
+						link = decodeURIComponent( segments[ i ] );
+					}
+					if ( i == segments.length - 1 ) {
+						crumb.classList.add( 'active', 'fg-active' )
+					}
+					crumb.append( link );
+					breadcrumbs += crumb.outerHTML;
+				}
+			}
+			breadcrumb.innerHTML = breadcrumbs;
+			const spinner = document.getElementById( 'loading-spinner' );
+			spinner.style.display = "none";
+			addEvent( breadcrumb, 'click', navigate );
+		}, 500 );
+	}
 }
 
 function navigate ( event ) {
-	if ( event.target.nodeName == 'A' && event.target.href.endsWith( '/' ) ) {
-		event.preventDefault();
+	console.log( event.target.href );
+	if ( event.target.nodeName == 'A' ) {
+		if ( event.target.href.endsWith( '/' ) ) {
+			event.preventDefault();
+		}
+	}
+
+	if ( event.defaultPrevented ) {
+		if ( req ) req.abort();
+
+		const spinner = document.getElementById( 'loading-spinner' );
+		spinner.style.display = "";
 		swapPage( event.target.href, event );
 	}
 }
 
 function swapPage ( href, event = undefined ) {
-	fetch( href )
+	req = new AbortController();
+	fetch( href, {
+			signal: req.signal
+		} )
 		.then( response => response.text() )
 		.then( data => {
-			var target = document.getElementById( 'list' );
-			var doc = document.implementation.createHTMLDocument();
-			doc.documentElement.innerHTML = data;
-			target.innerHTML = doc.getElementById( 'list' ).innerHTML;
+			document.documentElement.innerHTML = data;
+
+			updateCrumbs();
+
 			if ( event ) {
-				var title = event.target.innerHTML;
+				let title = event.target.innerHTML;
+				let path = event.target.pathname;
+				if ( path.endsWith( "/" ) )
+					window.document.title = "Files: " + decodeURI( path );
+				else
+					window.document.title = "View: " + decodeURI( path );
+
 				history.pushState( {
 					page: title
-				}, title, event.target.href );
+				}, title, href );
 			}
-			document.getElementById( 'search' ).value = '';
-			updateCrumbs();
-			table_row_link_class();
+
+			let search_bar = document.getElementById( 'search_bar' );
+			if ( search_bar ) {
+				setup_search_bar( search_bar );
+				document.getElementById( 'search' ).value = '';
+			}
+
+			let list = document.getElementById( 'list' );
+			if ( list ) table_row_link_class( list );
+
+			// scroll to top
+			document.body.scrollTop = 0;
+			document.documentElement.scrollTop = 0;
+			req = undefined;
+		} )
+		.catch( err => {
+			console.log( "Canceled navigation: " + href );
+			console.log( err );
+			req = undefined;
 		} );
 }
 
-function table_row_link_class () {
-	document.getElementById( 'list' ).className = 'table table-hover table-dark';
-	var headers = document.getElementsByTagName( 'th' );
-	var i;
+function table_row_link_class ( list ) {
+	list.className = 'table table-hover table-dark';
+	let headers = document.getElementsByTagName( 'th' );
+	let i;
 	for ( i = 0; i < headers.length; i++ ) {
 		headers[ i ].scope = "col";
 	}
-	var files = document.getElementById( 'list' ).getElementsByTagName( 'tbody' )[ 0 ].getElementsByTagName( 'a' );
+	let files = list.getElementsByTagName( 'tbody' )[ 0 ].getElementsByTagName( 'a' );
 	for ( i = 0; i < files.length; i++ ) {
 		if ( "classList" in files[ i ] ) files[ i ].classList.add( "text-info" );
 		else {
 			files[ i ].className = files[ i ].className.split( " " ).concat( "text-info" ).join( " " );
 		}
 	}
+	addEvent( list, 'click', navigate );
 }
-addEvent( window, 'popstate', e => {
-	swapPage( window.location.pathname );
-} );
-addEvent( window, 'DOMContentLoaded', e => {
-	updateCrumbs();
-	table_row_link_class();
-	var form = document.createElement( 'form' );
-	var input = document.createElement( 'input' );
+
+function setup_search_bar ( elem ) {
+	let form = document.createElement( 'form' );
+	let input = document.createElement( 'input' );
 	form.className = 'form';
 	form.setAttribute( 'onsubmit', "event.preventDefault();" );
 	input.name = 'filter';
@@ -112,9 +168,9 @@ addEvent( window, 'DOMContentLoaded', e => {
 	input.placeholder = 'Type to search...';
 	input.className = 'form-control text-light bg-dark';
 	form.appendChild( input );
-	document.getElementById( 'search_bar' ).appendChild( form );
-	input.addEventListener( 'keyup', function () {
-		var i,
+	elem.appendChild( form );
+	addEvent( input, 'keyup', function () {
+		let i,
 			e = "^(?=.*\\b" + this.value.trim().split( /\s+/ ).join( "\\b)(?=.*\\b" ) + ").*$",
 			n = RegExp( e, "i" );
 		Array.prototype.filter.call(
@@ -128,4 +184,19 @@ addEvent( window, 'DOMContentLoaded', e => {
 			item.hidden = true;
 		} );
 	} );
+}
+
+addEvent( window, 'popstate', e => {
+	swapPage( window.location.pathname );
+} );
+
+addEvent( window, 'DOMContentLoaded', e => {
+
+	updateCrumbs();
+
+	let list = document.getElementById( 'list' );
+	if ( list ) table_row_link_class( list );
+
+	let search_bar = document.getElementById( 'search_bar' );
+	if ( search_bar ) setup_search_bar( search_bar );
 } );
