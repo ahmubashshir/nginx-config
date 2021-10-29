244f3adcdc259867b878ad707be42dead5752fcd
Mubashshir <ahmubashshir@gmail.com>
2021-10-29 19:09:25 +0600

    Add license

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,13 +1,37 @@
-function setCookie ( cname, cvalue, exdays ) {
+/* jshint esversion: 6
+ *
+ * Utilities
+ *
+ * Copyright (C) 2021  Ahmad Hasan Mubashshir <ahmubashshir@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+function setCookie( cname, cvalue, exdays ) {
 	var d = new Date();
 	d.setTime( d.getTime() + ( exdays * 24 * 60 * 60 * 1000 ) );
 	var expires = "expires=" + d.toUTCString();
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-function setLTCookie(cname, cvalue) {
-	setCookie(cname, cvalue, 10980);
+
+function setLTCookie( cname, cvalue ) {
+	setCookie( cname, cvalue, 10980 );
 }
-function getCookie ( cname ) {
+
+function getCookie( cname ) {
 	var name = cname + "=";
 	var decodedCookie = decodeURIComponent( document.cookie );
 	var ca = decodedCookie.split( ';' );
@@ -23,7 +47,7 @@ function getCookie ( cname ) {
 	return "";
 }
 
-function getParam ( name ) {
+function getParam( name ) {
 	if ( name = ( new RegExp( '[?&]' + encodeURIComponent( name ) + '=([^&]*)' ) ).exec( location.search ) )
 		return decodeURIComponent( name[ 1 ] );
 }
--- static/js/theme-selector.js
+++ static/js/theme-selector.js
@@ -1,32 +1,55 @@
- // function to set a given theme/color-scheme
-import('./misc.js');
-function theme(){
-	return (window.hasOwnProperty('themecookie'))?window[themecookie]:'theme';
+/* jshint esversion: 6
+ *
+ * Dark/Light Theme toggle
+ * Copyright (C) 2021  Ahmad Hasan Mubashshir <ahmubashshir@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+// function to set a given theme/color-scheme
+import( './misc.js' );
+
+function theme() {
+	return ( window.hasOwnProperty( 'themecookie' ) ) ? window[ themecookie ] : 'theme';
 }
-function setTheme(themeName) {
-	setLTCookie(theme(), themeName);
+
+function setTheme( themeName ) {
+	setLTCookie( theme(), themeName );
 	if ( window.themes == undefined )
 		return;
-	if ( window.themes instanceof Object && window.themes.hasOwnProperty(themeName) )
-		document.getElementById(window.themestyle).href = window.themes[themeName];
+	if ( window.themes instanceof Object && window.themes.hasOwnProperty( themeName ) )
+		document.getElementById( window.themestyle ).href = window.themes[ themeName ];
 
 }
 
 // function to toggle between light and dark theme
 function toggleTheme() {
-	if (getCookie(theme()) === 'dark') {
-		setTheme('light');
+	if ( getCookie( theme() ) === 'dark' ) {
+		setTheme( 'light' );
 	} else {
-		setTheme('dark');
+		setTheme( 'dark' );
 	}
 }
 // Immediately invoked function to set the theme on initial load
-(function () {
-	if (getCookie(theme()) === 'dark') {
-		setTheme('dark');
-		document.getElementById('slider').checked = false;
+( function () {
+	if ( getCookie( theme() ) === 'dark' ) {
+		setTheme( 'dark' );
+		document.getElementById( 'slider' ).checked = false;
 	} else {
-		setTheme('light');
-	  	document.getElementById('slider').checked = true;
+		setTheme( 'light' );
+		document.getElementById( 'slider' ).checked = true;
 	}
-})();
+} )();
